677c61b2e2699b9c9667bdd1e5b6e40cbc18608e
Rudra P Patel <rudrapatel2833@gmail.com>
2024-07-22 21:31:41 +0530

    Commit-1 Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, changeColor] = useState("black");
+
+  const colorCode = ["red", "orange", "yellow", "green", "blue", "purple", "brown"];
+
+  function handleClick(e){
+    changeColor(e.target.id)
+  }
+
+  return (
+    <>
+    <div style={{backgroundColor:color}} className="w-full h-screen duration-200 grid gap-y-20 justify-center items-center">
+      <div className="flex justify-center items-center">
+      <h1 style={{color:"white"}}>Change Background!</h1>
+      </div>
+
+      <div className="flex flex-row justify-center">
+        {colorCode.map((ele)=>{
+          return <button id={ele} style={{backgroundColor:ele}} onClick={handleClick}>{ele}</button>
+        })}
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App
